200472e1799bad7fa6dd93290f61effbf9778911
Sejal-G <sejalcjm@gmail.com>
2020-11-30 16:20:05 +0530

    Remove noise and improve results

--- ocr_table.py
+++ ocr_table.py
@@ -1,8 +1,8 @@
-import cv2
+from cv2 import cv2
 import numpy as np
 import pytesseract
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Users\\DELL\\ocr\\Tesseract-OCR\\tesseract.exe'
 
 def ocr_core(img):
   text=pytesseract.image_to_string(img)
@@ -11,12 +11,22 @@ def ocr_core(img):
 
 img = cv2.imread('F_01.jpg')
 img=cv2.resize(img, (780, 540),  
-               interpolation = cv2.INTER_NEAREST) 
+               interpolation = cv2.INTER_NEAREST)
+               
+img = cv2.fastNlMeansDenoisingColored(img,None,None,None,None,None)
 
+#converting to grayscale image
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray,50,150,apertureSize = 3)
+cv2.imshow('gray',gray)
+
+#removing noise from binary image
+adaptive_threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 85, 11)
+cv2.imshow('adaptive_threshold',adaptive_threshold)
+
+#edges
+edges = cv2.Canny(adaptive_threshold,50,150,apertureSize = 3)
 cv2.imshow('edges', edges)
-lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength=100,maxLineGap=12)
+lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength=50,maxLineGap=120)
 for line in lines:
     x1,y1,x2,y2 = line[0]
     cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)

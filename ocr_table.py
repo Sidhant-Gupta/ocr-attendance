d55a0fc48468650261123838746bc49ccb0bea02
Sidhan-Gupta <guptasidhant04@gmail.com>
2020-12-01 12:42:18 +0530

    dhancha extracted

--- ocr_table.py
+++ ocr_table.py
@@ -8,7 +8,9 @@ def ocr_core(img):
   text=pytesseract.image_to_string(img)
   return text
 
-
+blankImg=cv2.imread('blank.png')
+blankImg=cv2.resize(blankImg, (780, 540),  
+               interpolation = cv2.INTER_NEAREST)
 img = cv2.imread('F_01.jpg')
 img=cv2.resize(img, (780, 540),  
                interpolation = cv2.INTER_NEAREST) 
@@ -16,17 +18,24 @@ img=cv2.resize(img, (780, 540),
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray,50,150,apertureSize = 3)
 cv2.imshow('edges', edges)
-lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength=100,maxLineGap=12)
+lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength=100,maxLineGap=10)
 for line in lines:
     x1,y1,x2,y2 = line[0]
     cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+    cv2.line(blankImg,(x1,y1),(x2,y2),(0,255,0),2)
 
-cv2.imshow('image', img)
+img1=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img2=cv2.cvtColor(blankImg, cv2.COLOR_BGR2GRAY)
+
+result_image = cv2.subtract(blankImg, img)
+cv2.imshow('subtractimage', result_image)
+# cv2.imshow('image', img)
+cv2.imshow('image', blankImg)
 
 # img = cv2.resize(img, None, fx=0.5, fy=0.5)
 # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # adaptive_threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 85, 11)
-print(ocr_core(img)) 
+print(ocr_core(result_image)) 
 
 
 
